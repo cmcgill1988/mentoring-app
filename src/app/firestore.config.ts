c9aac077446f29742af719f549e4e2cd1aafba9e
Connor McGill <connor.mcgill@critonapps.com>
2019-02-20 16:25:57 +0000

    Removes firestore config values to bring in line with database having been locked down

--- src/app/firestore.config.ts
+++ src/app/firestore.config.ts
@@ -1,8 +1,8 @@
 export const FIRESTORE_CONFIG = {
-  apiKey: 'AIzaSyDhyYRQ5JN-VaLx-GwYND78J0ZhXhRa9XU',
-  authDomain: 'mentoring-app-df6be.firebaseapp.com',
-  databaseURL: 'https://mentoring-app-df6be.firebaseio.com',
-  projectId: 'mentoring-app-df6be',
-  storageBucket: '',
-  messagingSenderId: '728455147948'
+  apiKey: 'XXX',
+  authDomain: 'XXX',
+  databaseURL: 'XXX',
+  projectId: 'XXX',
+  storageBucket: 'XXX',
+  messagingSenderId: 'XXX'
 };
